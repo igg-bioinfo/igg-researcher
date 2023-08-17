71d15cc90012734f198653460e559746441a4371
puva <paolouva@gaslini.org>
2023-08-17 15:55:05 +0200

    ome bulk update

--- 0_Home.py
+++ 0_Home.py
@@ -21,15 +21,35 @@ if investigator.update_date:
     st.write("Ultimo aggiornamento dei dati: **" + str(investigator.update_date) + "**")
 user_name = st.text_input("Email Gaslini / username:", value=investigator.user_name if investigator.user_name else "")
 first_name = st.text_input("Nome:", value=investigator.first_name if investigator.first_name else "")
-surname = st.text_input("Cognome:", value=investigator.last_name if investigator.last_name else "")
+last_name = st.text_input("Cognome:", value=investigator.last_name if investigator.last_name else "")
+if first_name == "" or last_name == "":
+    st.error("Compila il tuo nome e cognome correttamente")
 set_prop(st, "Età", investigator.age)
 contract = st.text_input("Contratto:", value=investigator.contract if investigator.contract else "")
 unit = st.text_input("Unità operativa:", value=investigator.unit if investigator.unit else "")
-#set_prop(st, "Contratto", investigator.contract)
 scopus_id = st.text_input("SCOPUS ID:", value=investigator.scopus_id if investigator.scopus_id else "")
+
 orcid_id = st.text_input("ORCID ID:", value=investigator.orcid_id if investigator.orcid_id else "")
 researcher_id = st.text_input("Researcher ID:", value=investigator.researcher_id if investigator.researcher_id else "")
-investigator.save_ids(first_name, surname, user_name, contract, scopus_id, orcid_id, researcher_id)
+investigator.save_ids(first_name, last_name, user_name, contract, scopus_id, orcid_id, researcher_id)
+st.markdown("---")
+
+if investigator.first_name != "" or investigator.last_name != "":
+    st.markdown("**Controlla su Scopus di non avere identificativi doppi a tuo nome al seguente link:**")
+    html = ""
+    html += "<a href='https://www.scopus.com/results/authorNamesList.uri?name=name&st1="
+    names = first_name.split(" ")
+    name_initials = []
+    for n in names:
+        name_initials.append(n[0])
+    html += last_name.replace(" ", "+") + "&st2=" + "+".join(name_initials) + "&origin=searchauthorlookup'>"
+    html += "Link a Scopus</a>"
+    st.markdown(html, unsafe_allow_html=True)
+    st.markdown("**In caso di identificativi multipli:**")
+    st.markdown("* Loggati a Scopus con le tue credenziali")
+    st.markdown("* Seleziona gli identificativi multipli")
+    st.markdown("* Clicca sull'opzione 'Request to merge authors'")
+    st.markdown("* Segui le istruzioni della banca dati")
 st.markdown("---")
 
 st.markdown("#### Metriche da Scopus")
@@ -61,7 +81,7 @@ with col_all:
 st.markdown("---")
 
 st.markdown("#### Pubblicazioni di Scopus")
-st.write("Pubblicazioni ricavate unicamente dalla banca dati Scopus (se registrato su Scopus viene riportato anche l'identificativo di PubMed PMID)")
+st.write("Pubblicazioni ricavate unicamente dalla banca dati Scopus")
 year_pubs = select_year(st, True)
 investigator.get_pubs(year_pubs)
 
